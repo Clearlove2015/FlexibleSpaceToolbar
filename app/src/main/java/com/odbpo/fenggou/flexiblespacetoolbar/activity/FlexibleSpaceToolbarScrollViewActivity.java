cb1afb8abced28b35cc8965acbca39fef8484746
zhucheng <zhucheng@odbpo.com>
2018-08-01 16:34:14 +0800

    init commit

---
+++ app/src/main/java/com/odbpo/fenggou/flexiblespacetoolbar/activity/FlexibleSpaceToolbarScrollViewActivity.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.odbpo.fenggou.flexiblespacetoolbar.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.nineoldandroids.view.ViewHelper;
+import com.odbpo.fenggou.flexiblespacetoolbar.R;
+import com.odbpo.fenggou.flexiblespacetoolbar.scroll.ObservableScrollView;
+import com.odbpo.fenggou.flexiblespacetoolbar.scroll.ObservableScrollViewCallbacks;
+import com.odbpo.fenggou.flexiblespacetoolbar.scroll.ScrollState;
+import com.odbpo.fenggou.flexiblespacetoolbar.scroll.ScrollUtils;
+
+public class FlexibleSpaceToolbarScrollViewActivity extends BaseActivity implements ObservableScrollViewCallbacks {
+
+    private View mFlexibleSpaceView;
+    private View mToolbarView;
+    private TextView mTitleView;
+    private int mFlexibleSpaceHeight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flexiblespacetoolbarscrollview);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        mFlexibleSpaceView = findViewById(R.id.flexible_space);
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText(getTitle());
+        setTitle(null);
+        mToolbarView = findViewById(R.id.toolbar);
+
+        final ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        scrollView.setScrollViewCallbacks(this);
+
+        mFlexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_height);
+        int flexibleSpaceAndToolbarHeight = mFlexibleSpaceHeight + getActionBarSize();
+
+        findViewById(R.id.body).setPadding(0, flexibleSpaceAndToolbarHeight, 0, 0);
+        mFlexibleSpaceView.getLayoutParams().height = flexibleSpaceAndToolbarHeight;
+
+        ScrollUtils.addOnGlobalLayoutListener(mTitleView, new Runnable() {
+            @Override
+            public void run() {
+                updateFlexibleSpaceText(scrollView.getCurrentScrollY());
+            }
+        });
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        updateFlexibleSpaceText(scrollY);
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+
+    }
+
+    private void updateFlexibleSpaceText(final int scrollY) {
+        System.out.println("scrollY:" + scrollY);
+        //translationX和translationY：这些属性控制View所在的位置，作为其布局容器设置的坐标left和top坐标。
+        ViewHelper.setTranslationY(mFlexibleSpaceView, -scrollY);
+        int adjustedScrollY = (int) ScrollUtils.getFloat(scrollY, 0, mFlexibleSpaceHeight);
+        float maxScale = (float) (mFlexibleSpaceHeight - mToolbarView.getHeight()) / mToolbarView.getHeight();
+        float scale = maxScale * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight;
+
+        //pivotX和pivotY：这些属性控制枢轴点的位置，围绕该枢轴点进行旋转和缩放变换。默认情况下，轴心点位于对象中心的中心。
+        ViewHelper.setPivotX(mTitleView, 0);
+        ViewHelper.setPivotY(mTitleView, 0);
+        //scaleX和scaleY：这些属性控制View围绕其轴心点的2D缩放。
+        ViewHelper.setScaleX(mTitleView, 1 + scale);
+        ViewHelper.setScaleY(mTitleView, 1 + scale);
+        int maxTitleTranslationY = mToolbarView.getHeight() + mFlexibleSpaceHeight - (int) (mTitleView.getHeight() * (1 + scale));
+        int titleTranslationY = (int) (maxTitleTranslationY * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight);
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+    }
+}
